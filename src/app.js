a3a175cd0604bfb94e2090cab334a0a38cf8163b
Felipe Monteiro <luizfelipe.p@live.com>
2020-04-08 22:19:18 -0300

    Challenge-Complete

--- src/app.js
+++ src/app.js
@@ -13,22 +13,88 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const likes = 0
+  const id = uuid()
+
+  const {title, url, techs} = request.body
+
+  const result = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories.push(result)
+
+  return response.json(result)
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params
+  const {title, url, techs} = request.body
+
+  const index = repositories.findIndex(repository => repository.id === id)
+
+  if (index < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  const likes = repositories[index].likes
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[index] = repository
+
+  return response.json(repository)
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
+app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params
+
+  const index = repositories.findIndex(repository => repository.id === id)
+
+  if (index < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  repositories.splice(index, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const { id } = request.params
+
+  const index = repositories.findIndex(repository => repository.id === id)
+
+  if (index < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  let like = repositories[index].likes
+  like += 1
+
+  repositories[index].likes = like
+
+  return response.json(repositories[index])
 });
 
 module.exports = app;
